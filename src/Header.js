8405eaeee87a940f048a64177e13c1f3e447d95a
Timilehin Bello <emmatimmybello@gmail.com>
2023-08-01 12:07:21 +0100

    feat: consume api

---
+++ src/Header.js
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { UserContext } from "./UserContext";
+
+export default function Header() {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+  useEffect(() => {
+    fetch("http://localhost:8080/api/user/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, [setUserInfo]);
+
+  function logout() {
+    fetch("http://localhost:8080/api/user/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">
+        MyBlog
+      </Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a href onClick={logout}>
+              Logout ({username})
+            </a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
